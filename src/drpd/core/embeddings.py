3fd202ba4b8b78068391ecd2d4af0e9fc2b48eb5
khanh.huu <khanh.huu@mobifone.vn>
2025-12-02 16:37:46 +0700

    feat: add embedding service

---
+++ src/drpd/core/embeddings.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from typing import Union, List
+import numpy as np
+import requests
+from drpd.config import app_config
+
+class BaseEmbedding(ABC):
+    """Abstract base class for embedding managers."""
+    @abstractmethod
+    def embed(self, texts: Union[str, List[str]], normalize: bool = True, **kwargs) -> np.ndarray:
+        """Convert text into embedding."""
+        pass
+
+
+# TODO
+# Local LLM
+
+class EmbeddingModel(BaseEmbedding):
+    """Embedding using remote url."""
+    def __init__(self):
+        """Initialize the embedding manager."""
+        self.url = app_config["embedding"]["url"]
+        self.model = app_config["embedding"]["model"]
+    
+    def embed(self, texts, normalize = True, batch_size = 32) -> np.ndarray:
+        """Generate embedding for input text."""
+        if isinstance(texts, str):
+            texts = [texts]
+        
+        import concurrent.futures
+
+        def _process_batch(batch_texts):
+            try:
+                response = requests.post(
+                    self.url,
+                    headers={
+                        "accept": "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    json={
+                        "model": self.model,
+                        "input": batch_texts,
+                    }
+                )
+                response.raise_for_status()
+                data = response.json()
+                return [item["embedding"] for item in data["data"]]
+            except Exception as e:
+                raise RuntimeError(f"Embedding failed for batch {e}") from e
+        
+        batches = [texts[i : i + batch_size] for i in range(0, len(texts), batch_size)]
+        results = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            results = list(executor.map(_process_batch, batches))
+
+        flat_embeddings = [emb for batch_result in results for emb in batch_result]
+
+        return np.array(flat_embeddings)
+        
+encoder = EmbeddingModel()
